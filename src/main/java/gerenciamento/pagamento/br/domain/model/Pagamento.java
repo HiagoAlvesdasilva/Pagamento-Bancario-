fd6343f84869387005763950d9f50e213a8d9cbe
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2024-11-05 15:36:43 -0300

    Feat: Criando Abstract Pagamento

---
+++ src/main/java/gerenciamento/pagamento/br/domain/model/Pagamento.java
@@ -0,0 +1,21 @@
+package gerenciamento.pagamento.br.domain.model;
+
+public abstract class Pagamento {
+    private double valor;
+    private String status;
+
+    public Pagamento(double valor, String status) {
+        this.valor = valor;
+        this.status = "Pendente";
+    }
+
+    public abstract void realizarPagamento();
+
+    public String verificarStatus() {
+        return status;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
